dbf8788a205b55082cf72278da97cae1a0397caa
cengels <cengels@tuta.io>
2020-06-02 19:58:06 +0200

    Add deploy script
    It doesn't work for me due to an internal compiler error, but maybe
    it'll work for someone else.

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Deploys the application on Windows.
+# Make sure Makefile.Release exists.
+# You may have to adjust the Qt paths on your system.
+
+
+cd build
+
+"/mnt/e/Qt/5.14.2/mingw73_64/bin/qmake.exe" ../skywriter.pro
+"/mnt/e/Qt/Tools/mingw730_64/bin/mingw32-make.exe" -f Makefile.Release
+"/mnt/e/Qt/5.14.2/mingw73_64/bin/windeployqt.exe" release --qmldir ../resources/qml
+
+cd ..

1f209a64811084b92d0a1a2d70435eaaf2ad0533
cengels <cengels@tuta.io>
2020-10-17 13:37:20 +0200

    Fix incorrect hex stringification
    Previously, toHexadecimal() simply iterated through each QChar in a
    string and converted it to hex. This is not actually a valid approach
    due to the fact that a QChar can actually be multiple bytes long, for
    example the double curly quote character (“), which corresponds to e2 80
    9c. These kinds of characters were incorrectly converted. By using a
    QByteArray, this problem no longer occurs.

--- test/unit/customqtprint.h
+++ test/unit/customqtprint.h
@@ -9,19 +9,22 @@
 // Defines custom print functions for Qt types to better integrate Qt with
 // gtest and allow for more readable test assertion failure messages.
 
-std::string toHexadecimal(const QString& string) {
-    std::stringstream outerStream;
+namespace {
+    std::string toHexadecimal(const QString& string) {
+        QByteArray byteArray = string.toUtf8();
+        std::stringstream outerStream;
 
-    for (const QChar& character : string) {
-        std::stringstream stream;
-        stream << std::hex << std::setfill('0') << std::setw(4) << (int)character.toLatin1();
-        outerStream << stream.str() << " ";
+        for (const char& character : byteArray) {
+            std::stringstream stream;
+            stream << std::hex << std::setfill('0') << std::setw(4) << (int)character;
+            outerStream << stream.str() << " ";
+        }
+
+        std::string result = outerStream.str();
+        result.pop_back();
+
+        return result;
     }
-
-    std::string result = outerStream.str();
-    result.pop_back();
-
-    return result;
 }
 
 QT_BEGIN_NAMESPACE
